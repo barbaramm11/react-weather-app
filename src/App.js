d517b30fb7cb17420a4467d82960c7c0b9915d6e
barbaramm11 <barbaramm11@gmail.com>
2022-09-03 18:42:23 -0500

    added html

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
-
+import weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Barbara Morales and is{" "}
-        <a href="https://github.com/barbaramm11/react-weather-app">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <weather />
+        <footer>
+          This project was coded by Barbara Morales and is{" "}
+          <a href="https://github.com/barbaramm11/react-weather-app">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
