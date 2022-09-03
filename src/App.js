418f49ed692bfe03e4e256b286f68f2d646cd5d4
barbaramm11 <barbaramm11@gmail.com>
2022-09-02 21:49:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by Barbara Morales and is{" "}
-        <a
-          href="https://github.com/barbaramm11/react-weather-app"
-          target="_blank"
-        >
+        <a href="https://github.com/barbaramm11/react-weather-app">
           open-sourced on GitHub
         </a>
       </footer>
