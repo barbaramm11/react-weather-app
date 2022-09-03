23ed1e7dea11d497316dde6235016637c6c8dba2
barbaramm11 <barbaramm11@gmail.com>
2022-09-02 21:45:33 -0500

    added Github URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
+
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Barbara Morales and is{" "}
+        <a
+          href="https://github.com/barbaramm11/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+      ;
+    </div>
+  );
 }
-
-export default App;
