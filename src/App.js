28e98f28b4a174a73ed998b1735a2600bda8b4ee
barbaramm11 <barbaramm11@gmail.com>
2022-09-03 19:27:17 -0500

    updated content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
-import weather from "./Weather";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <weather />
+        <Weather defaultCity="New York" />
         <footer>
           This project was coded by Barbara Morales and is{" "}
           <a href="https://github.com/barbaramm11/react-weather-app">
             open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://legendary-salmiakki-c4fd40.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
           </a>
         </footer>
       </div>
