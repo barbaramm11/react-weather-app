d94ebdaac6b236bbed96a196b2288f66624e03d3
barbaramm11 <barbaramm11@gmail.com>
2022-09-03 19:29:50 -0500

    updated and installed npm install react-animated-weather

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -29,7 +29,7 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.map(function (dailyForecast, index) {
+          {forecast.map(function(dailyForecast, index) {
             if (index < 5) {
               return (
                 <div className="col" key={index}>
